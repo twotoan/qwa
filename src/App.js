7d69477488bb012c71de0cb162f7c6b9eb4d8909
Matthew Davies <matthew.davies@Revive.local>
2018-07-17 16:02:50 +0100

    Different routing approach

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import './App.css';
-import Dashboard from './components/Dashboard/Dashboard';
-import Navigation from "./components/Layout/Navigation";
-import LoginForm from "./components/Login/LoginForm";
+import LoginForm from './components/Login/LoginForm';
 
 class App extends Component {
     render() {
         return (
-            <Router>
-                <div>
-                    <LoginForm/>
-                    {/*<Navigation/>*/}
-
-                    <Route path="/dashboard" component={Dashboard} />
-                    {/*<Route path="/quote" component={Quote} />*/}
-                </div>
-            </Router>
-
+            <LoginForm />
         );
     }
 }
--- src/components/Layout/Navigation.js
+++ src/components/Layout/Navigation.js
@@ -1,18 +1,17 @@
 import React, {Component} from "react";
-import {Link} from "react-router-dom";
 
 class Navigation extends Component {
     render() {
         return (
-
             <div>
                 <ul>
                     <li>
-                        <Link to="/dashboard">Home</Link>
+                        <a href="/dashboard">Dashboard</a>
+                        <a href="/details">Conversion Statistics</a>
+                        <a href="/logout">Logout</a>
                     </li>
                 </ul>
             </div>
-
         )
     }
 }
